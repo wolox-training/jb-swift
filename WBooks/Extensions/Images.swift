178198e5bae2f45e643c6378346ffb3b62972eb7
Joaquín Bozzalla <joaco.mbb@gmail.com>
2021-04-12 17:02:07 -0300

    Images and labels reusability in tabbar section.

--- WBooks/Extensions/Images.swift
+++ WBooks/Extensions/Images.swift
@@ -8,8 +8,13 @@
 import UIKit
 
 extension UIImage {
+    // Navigation Bar icons
     static let icNotifications = UIImage(named: "ic_notifications")
     static let icSearch = UIImage(named: "ic_search")
-    
-    
+    // TabBar Icons
+    static let icLibrary = UIImage(named: "ic_library")
+    static let icWishlist = UIImage(named: "ic_wishlist")
+    static let icAddNew = UIImage(named: "ic_add new")
+    static let icMyRentals = UIImage(named: "ic_myrentals")
+    static let icSettings = UIImage(named: "ic_settings")
 }
