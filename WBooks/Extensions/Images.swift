025f3d2cfaba1c63db4e520ce22b4e9e2d0ac648
Joaquín Bozzalla <joaco.mbb@gmail.com>
2021-04-14 12:51:48 -0300

    Code style improvements

--- WBooks/Extensions/Images.swift
+++ WBooks/Extensions/Images.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 extension UIImage {
-    // Navigation Bar icons
+    // MARK: - Navigation Bar icons
     static let notifications = UIImage(named: "ic_notifications.png")
     static let search = UIImage(named: "ic_search.png")
-    // TabBar Icons
+    // MARK: - TabBar Icons
     static let library = UIImage(named: "ic_library")
     static let wishlist = UIImage(named: "ic_wishlist")
     static let addNew = UIImage(named: "ic_add new")
