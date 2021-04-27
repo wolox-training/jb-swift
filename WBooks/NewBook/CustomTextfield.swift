3130a8815f8b3bf289bdd7141464fd89047cb0eb
Joaquín Bozzalla <joaco.mbb@gmail.com>
2021-04-27 11:00:48 -0300

    Code improvement

--- WBooks/NewBook/CustomTextfield.swift
+++ WBooks/NewBook/CustomTextfield.swift
@@ -9,6 +9,7 @@ import UIKit
 
 @IBDesignable
 open class CustomTextfield: UITextField {
+    private let textfieldColor = UIColor.systemGray3
     var validator: ((String) -> Bool)?
     
     public override init(frame: CGRect) {
@@ -25,12 +26,12 @@ open class CustomTextfield: UITextField {
         addTarget(self, action: #selector(isValid), for: .editingChanged)
         
         self.attributedPlaceholder = NSAttributedString(string: self.placeholder ?? "", attributes: [
-            NSAttributedString.Key.foregroundColor: UIColor.newBookTextfield
+            NSAttributedString.Key.foregroundColor: textfieldColor
         ])
         self.borderStyle = .none
         self.layer.backgroundColor = UIColor.white.cgColor
         self.layer.masksToBounds = false
-        self.layer.shadowColor = UIColor.newBookTextfield.cgColor
+        self.layer.shadowColor = textfieldColor.cgColor
         self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
         self.layer.shadowOpacity = 1.0
         self.layer.shadowRadius = 0.0
@@ -39,7 +40,7 @@ open class CustomTextfield: UITextField {
     @objc func isValid() -> Bool {
         if let validatorFunction = validator, let text = self.text {
             let result = validatorFunction(text)
-            self.layer.shadowColor = result ? UIColor.newBookTextfield.cgColor : UIColor.red.cgColor
+            self.layer.shadowColor = result ? textfieldColor.cgColor : UIColor.red.cgColor
             return result
         }
         return true
